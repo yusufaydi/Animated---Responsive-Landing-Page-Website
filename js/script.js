7fda6e3ca20c2c78949cd006d0b5c1dc15cac4dc
yusufaydi <bm.yusufaydin@gmail.com>
2024-01-24 13:34:23 +0300

    Animated-Movie-Website

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function toglleVideo() {
+  const trailer = document.querySelector(".trailer");
+  const video = document.querySelector("video");
+  video.pause();
+  trailer.classList.toggle("active");
+}
+
+function changeBg(bg, title) {
+  const banner = document.querySelector(".banner");
+  const contents = document.querySelectorAll(".content");
+  banner.style.background = `url("../images/${bg}") no-repeat`;
+  banner.style.objectFit = "cover";
+  banner.style.objectPosition = "position";
+  banner.style.backgroundsize = "cover";
+  banner.style.backgroundPosition = "center";
+  /*
+  for (let i = 0; i < contents.length; i++) {
+    contents[i].classList.remove("active");
+    if (contents[i].classList.contains(title)) {
+      contents[i].classList.add("active");
+    }
+  }
+  */
+
+  contents.forEach((content) => {
+    content.classList.remove("active");
+    if (content.classList.contains(title)) {
+      content.classList.add("active");
+    }
+  });
+}
